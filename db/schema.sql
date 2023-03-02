1a46559f2acca43ba675882ec76dc5f88f2e0bbb
Tyler <tyler.dins88@gmail.com>
2023-03-01 22:44:12 -0600

    initial files created, schema.sql started

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    -- need to connect next line to create table department id
+    department_id INT 
+)
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+)
