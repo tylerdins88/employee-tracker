21651cffeb25d47724e15cab19d67aa263995d98
Tyler <tyler.dins88@gmail.com>
2023-03-05 18:53:58 -0600

    working on primary and foreign keys, schema

--- db/schema.sql
+++ db/schema.sql
@@ -10,13 +10,13 @@ SELECT DATABASE();
 
 -- Creating the departments table. 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 -- Creating the roles table. 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_name VARCHAR(30) NOT NULL -- link to department table department_name
@@ -24,11 +24,11 @@ CREATE TABLE role (
 
 -- Creating the employees table. 
 CREATE TABLE employee (
-    id INT PRIMARY KEY, -- link to role table id
+    id INT AUTO_INCREMENT PRIMARY KEY, -- link to role table id
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    title, -- link to role table title. 
+    title VARCHAR(30) NOT NULL, -- link to role table title. 
     department_name VARCHAR(30) NOT NULL, -- link to department table department_name
-    salary DECIMAL NOT NULL, -- link to role table salary
-    manager_id INT NOT NULL -- do i need to link this to person? 
+    salary DECIMAL(), -- link to role table salary
+    manager_id VARCHAR() -- do i need to link this to person? 
 );
